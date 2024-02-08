2a233eb0d2dcd1a9e7e3daed9decd566db0d40d5
AlexGthr <agauthier5@outlook.fr>
2024-02-08 09:57:11 +0100

    Ajout de la gestion du localStorage, via le tableau favorisInStorage en cliquant sur les coeurs.

--- js/main.js
+++ js/main.js
@@ -3,37 +3,43 @@
 // On récupère l'élément #board
 const board = document.querySelector(".board");
 
-const boxCitation = document.createElement("div")
+// Je crée ma div avec la classe .boxCitation
+const boxCitation = document.createElement("div") 
 boxCitation.classList.add("boxCitation")
 
-quotes.forEach((quote) => {
+// Je déclare une variable qui recupèrera le contenu de mon localStorage 'favoris' et si il est vide il sera un tableau
+let favorisInStorage = JSON.parse(localStorage.getItem('favoris')) // Je recupère ici mes favoris du localStorage
+if (favorisInStorage === null) { // Si c'est vide,
+    favorisInStorage = []; // alors ça sera un tableau
+}
+
+
+quotes.forEach((quote) => { // Je déclare une boucle pour la création des citations
     const newBox = boxCitation.cloneNode(); // Clone des divs
     let text = `<h1>${quote.title}</h1>
         <p class='author'>${quote.content}</p>
         <p>${quote.author}</p>
         <i id="coeur" class="fa-solid fa-heart"></i>`
          
-    newBox.innerHTML = text
+    newBox.innerHTML = text // J'ajoute l'html a la box
     board.appendChild(newBox);
 
-})
+    let coeur = newBox.querySelector(".fa-heart") // Je récupère les coeurs de la citation
 
+    if (favorisInStorage.some(favori => favori.id === quote.id)) { // Je vérifie si dans le tableau FavorisInStorage je possède des titres qui correspondent au quote
+        coeur.classList.add("clicked"); // Si c'est le cas, je leurs donne tout de suite la class clicked. (Permet à l'utilisateur qui à mis en favoris des quotes de revoir tout de suite ses favoris)
+    }
 
-    // let i = 0;
+    coeur.addEventListener("click", function() { // Je crée un eventlistener au click sur les coeurs de mes citations
+        coeur.classList.toggle("clicked"); // au click elles prendront la classe "clicked"
 
-    // if (localStorage.getItem() != null) {
-    //     coeur.classList.add(localStorage.getItem(coeur[i]))
-    // }
-    
-    // coeur.addEventListener("click", function() {
+        if(coeur.classList.contains("clicked")) { // Si elles sont cliqués la première fois, alors je les ajoutes dans mon tableau favorisInStorage
+            favorisInStorage.push(quote);
+        }
+        else {
+            favorisInStorage = favorisInStorage.filter(q => q.id !== quote.id); // Si elle sont cliqués une deuxieme fois, je les retires du tableau favorisInStorage
+        }
 
-    //     if (localStorage.getItem() != null) {
-
-    //         coeur.classList.remove("clicked")
-
-    //     } 
-        
-    //     else {
-    //         localStorage.setItem(, "clicked")
-    //         coeur.classList.add("clicked")
-    //     }
+        localStorage.setItem('favoris', JSON.stringify(favorisInStorage)) // Puis je rajoute dans le Storage ma clé 'favoris' qui contiendra le tableau 'favorisInStorage'
+    })
+    })
